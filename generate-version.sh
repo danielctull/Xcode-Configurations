b9f2315713c9643687e868ec1eb8f9c109ad0e6d
Daniel Tull <dt@danieltull.co.uk>
2016-09-29 12:31:58 +0530

    Remove generation of ReleaseVersion and DebugVersion
    With conditional imports in Xcode 8, we can optionally include the generated Version.xcconfig allowing us to just use the Release and Debug configuration files and have the version included if it has been generated.

--- generate-version.sh
+++ generate-version.sh
@@ -5,20 +5,8 @@ cd "${PROJECT_DIR}"
 
 bundleVersion=`git log --oneline | wc -l | tr -d ' '`
 configuration="${configurationDirectory}/Version.xcconfig"
-debugConfiguration="${configurationDirectory}/DebugVersion.xcconfig"
-releaseConfiguration="${configurationDirectory}/ReleaseVersion.xcconfig"
 
 rm -f "$configuration"
-rm -f "$debugConfiguration"
-rm -f "$releaseConfiguration"
 touch "$configuration"
-touch "$debugConfiguration"
-touch "$releaseConfiguration"
 
 echo "CURRENT_PROJECT_VERSION = $bundleVersion" >> "$configuration"
-
-echo "#include \"Debug.xcconfig\"" >> "$debugConfiguration"
-echo "#include \"Version.xcconfig\"" >> "$debugConfiguration"
-
-echo "#include \"Release.xcconfig\"" >> "$releaseConfiguration"
-echo "#include \"Version.xcconfig\"" >> "$releaseConfiguration"
